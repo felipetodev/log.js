745bddba60e720bd3d5e7ff5d4cf540c9cedff1f
felipetodev <fe.ossandon.u@gmail.com>
2024-10-17 15:31:08 -0300

    feat: add resizable editor panels

--- app/components/playground.tsx
+++ app/components/playground.tsx
@@ -1,6 +1,11 @@
 import { useMonacoEditor } from "~/hooks/use-monaco-editor";
 import { useTabsStore } from "~/store/tabs";
 import { LanguageSwitch } from "./language-switch";
+import {
+  Panel,
+  PanelGroup,
+  PanelResizeHandle
+} from "react-resizable-panels";
 
 export function Playground() {
   const {
@@ -12,12 +17,17 @@ export function Playground() {
   return (
     <>
       {hasHydratedStorage ? (
-        <div className="grid grid-cols-2 flex-col h-[calc(100%-56px)] bg-[#282A36]">
-          <div className="relative">
-            <div ref={inputEditorRef} className="size-full" />
-            <LanguageSwitch />
-          </div>
-          <div ref={outputEditorRef} className="size-full" />
+        <div className="h-[calc(100%-56px)] bg-[#282A36]">
+          <PanelGroup autoSaveId="editor" direction="horizontal">
+            <Panel defaultSize={50} minSize={20} className="relative">
+              <div ref={inputEditorRef} className="size-full" />
+              <LanguageSwitch />
+            </Panel>
+            <PanelResizeHandle />
+            <Panel defaultSize={50} minSize={20}>
+              <div ref={outputEditorRef} className="size-full" />
+            </Panel>
+          </PanelGroup>
         </div>
       ) : (
         <div className="grid h-[calc(100%-56px)] place-content-center bg-[#282A36]">
